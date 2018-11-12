54d2ffbcc6cd22a6c3aba1cee8bd483c9636b75d
Jörg Thalheim <joerg@thalheim.io>
2018-11-12 16:37:36 +0000

    add openwrt

---
+++ envs/openwrt/README.md
@@ -0,0 +1,4 @@
+## OpenWRT
+
+Build environment to bootstrap [OpenWRT buildroot](https://openwrt.org/)
+For more information take a look at: https://nixos.wiki/wiki/OpenWRT
---
+++ envs/openwrt/shell.nix
@@ -0,0 +1,46 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  fixWrapper = pkgs.runCommand "fix-wrapper" {} ''
+    mkdir -p $out/bin
+    for i in ${pkgs.gcc.cc}/bin/*-gnu-gcc*; do
+      ln -s ${pkgs.gcc}/bin/gcc $out/bin/$(basename "$i")
+    done
+    for i in ${pkgs.gcc.cc}/bin/*-gnu-{g++,c++}*; do
+      ln -s ${pkgs.gcc}/bin/g++ $out/bin/$(basename "$i")
+    done
+  '';
+
+  fhs = pkgs.buildFHSUserEnv {
+    name = "openwrt-env";
+    targetPkgs = pkgs: with pkgs; [
+      git
+      perl
+      gnumake
+      gcc
+      unzip
+      utillinux
+      python2
+      patch
+      wget
+      file
+      subversion
+      which
+      pkgconfig
+      openssl
+      fixWrapper
+      systemd
+      binutils
+
+      ncurses
+      zlib
+      zlib.static
+      glibc.static
+    ];
+    multiPkgs = null;
+    extraOutputsToInstall = [ "dev" ];
+    profile = ''
+      export hardeningDisable=all
+    '';
+  };
+in fhs.env
