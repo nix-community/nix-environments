92b506039cff6edfa8ed74fab5bc4d89b807e72a
Jörg Thalheim <joerg@thalheim.io>
2018-11-12 16:48:41 +0000

    add spec benchmark

---
+++ envs/spec/README.md
@@ -0,0 +1,4 @@
+## SPEC benchmark
+
+Build environment to build [SPEC](https://www.spec.org/benchmarks.html).
+At the time of writing only Spec CPU 2017 was tested.
---
+++ envs/spec/shell.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  specgcc = pkgs.gcc6;
+
+  fixWrapper = pkgs.runCommand "fix-wrapper" {} ''
+    mkdir -p $out/bin
+    for i in ${specgcc.cc}/bin/*-gnu-gcc*; do
+      ln -s ${specgcc}/bin/gcc $out/bin/$(basename "$i")
+    done
+    for i in ${specgcc.cc}/bin/*-gnu-{g++,c++}*; do
+      ln -s ${specgcc}/bin/g++ $out/bin/$(basename "$i")
+    done
+  '';
+
+  fhs = pkgs.buildFHSUserEnv {
+    name = "spec-env";
+    targetPkgs = pkgs: with pkgs; [
+      # higher priority then the default gcc
+      (hiPrio specgcc)
+      perl
+      gnumake
+      gfortran
+      gnutar
+      which
+      fixWrapper
+      binutils
+    ];
+    multiPkgs = null;
+    extraOutputsToInstall = [ "dev" ];
+    profile = ''
+      export hardeningDisable=all
+    '';
+  };
+in fhs.env
