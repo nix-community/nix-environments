c5bb62b0755b837576f9c8f7036edc348845482d
Jörg Thalheim <joerg@thalheim.io>
2019-02-05 20:49:02 +0000

    default.nix: also add github-pages

--- default.nix
+++ default.nix
@@ -2,4 +2,5 @@
   openwrt = import ./envs/openwrt/shell.nix { inherit pkgs; };
   phoronix-test-suite = import ./envs/phoronix-test-suite/shell.nix { inherit pkgs; };
   spec-benchmark = import ./envs/spec-benchmark/shell.nix { inherit pkgs; };
+  github-pages = import ./envs/github-pages/shell.nix { inherit pkgs; };
 }
