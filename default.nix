0eb7287187fd7119752a2ac38ec3eaedf30bbdde
Jörg Thalheim <joerg@thalheim.io>
2019-02-05 20:48:23 +0000

    rename spec to spec-benchmark

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }: {
   openwrt = import ./envs/openwrt/shell.nix { inherit pkgs; };
   phoronix-test-suite = import ./envs/phoronix-test-suite/shell.nix { inherit pkgs; };
-  spec = import ./envs/spec/shell.nix { inherit pkgs; };
+  spec-benchmark = import ./envs/spec-benchmark/shell.nix { inherit pkgs; };
 }
