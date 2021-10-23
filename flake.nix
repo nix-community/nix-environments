d39652afdda9a0f738a956345b3a4e73bcec6f17
Jörg Thalheim <joerg@thalheim.io>
2021-10-23 14:05:47 +0200

    build flake.nix from default.nix attr set

--- flake.nix
+++ flake.nix
@@ -3,32 +3,10 @@
 
   outputs = { self, nixpkgs }:
     let
-      forAllSystems = f: nixpkgs.lib.genAttrs
-        [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
-        (system: f system);
-      nixpkgsFor = forAllSystems (system:
-        import nixpkgs {
-          inherit system;
-        }
-      );
-    in
-    {
-
-      devShells = forAllSystems
-        (system:
-          let
-            pkgs = nixpkgsFor.${system};
-          in
-          {
-            arduino = import ./envs/arduino/shell.nix { inherit pkgs; };
-            github-pages = import ./envs/github-pages/shell.nix { inherit pkgs; };
-            home-assistant = import ./envs/home-assistant/shell.nix { inherit pkgs; };
-            nannou = import ./envs/nannou/shell.nix { inherit pkgs; };
-            jruby = import ./envs/jruby/shell.nix { inherit pkgs; };
-            openwrt = import ./envs/openwrt/shell.nix { inherit pkgs; };
-            phoronix-test-suite = import ./envs/phoronix-test-suite/shell.nix { inherit pkgs; };
-            spec-benchmark = import ./envs/spec-benchmark/shell.nix { inherit pkgs; };
-          });
-
+      systems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in {
+      devShells = nixpkgs.lib.genAttrs systems (system: import ./. {
+        pkgs = import nixpkgs { inherit system; };
+      });
     };
 }
