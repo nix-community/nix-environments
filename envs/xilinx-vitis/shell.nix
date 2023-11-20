9a9e652513a9a24f607ef31531f7e631d020872b
Jörg Thalheim <joerg@thalheim.io>
2023-11-20 10:57:53 +0100

    xilinx-vitis: add fixes for libidn/ncurses and fix eval

--- envs/xilinx-vitis/shell.nix
+++ envs/xilinx-vitis/shell.nix
@@ -1,5 +1,5 @@
-{ pkgs ? import <nixpkgs> {}
-, extraPkgs ? []
+{ pkgs ? import <nixpkgs> { }
+, extraPkgs ? [ ]
 , runScript ? "bash"
 , xilinxName ? "xilinx-env"
 }:
@@ -7,13 +7,23 @@
 (pkgs.buildFHSUserEnv {
   name = xilinxName;
   inherit runScript;
-  targetPkgs = pkgs: with pkgs; [
+  targetPkgs = pkgs: with pkgs; let
+    ncurses' = ncurses5.overrideAttrs (old: {
+      configureFlags = old.configureFlags ++ [ "--with-termlib" ];
+      postFixup = "";
+    });
+  in
+  [
     bash
     coreutils
     zlib
     lsb-release
     stdenv.cc.cc
-    ncurses5
+    # https://github.com/NixOS/nixpkgs/issues/218534
+    # postFixup would create symlinks for the non-unicode version but since it breaks
+    # in buildFHSUserEnv, we just install both variants
+    ncurses'
+    (ncurses'.override { unicodeSupport = false; })
     xorg.libXext
     xorg.libX11
     xorg.libXrender
@@ -31,6 +41,14 @@
     libxcrypt-legacy # required for Vivado
     python3
 
+    (libidn.overrideAttrs (_old: {
+      # we need libidn.so.11 but nixpkgs has libidn.so.12
+      src = fetchurl {
+        url = "mirror://gnu/libidn/libidn-1.34.tar.gz";
+        sha256 = "sha256-Nxnil18vsoYF3zR5w4CvLPSrTpGeFQZSfkx2cK//bjw=";
+      };
+    }))
+
     # to compile some xilinx examples
     opencl-clhpp
     ocl-icd
@@ -42,7 +60,7 @@
     unzip
     nettools
   ] ++ extraPkgs;
-  multiPkgs = null;
+  multiPkgs = ps: [];
   profile = ''
     export LC_NUMERIC="en_US.UTF-8"
     source /opt/xilinx/Vitis/*/settings64.sh
