344915f1eaac199306a2fa212026921734399308
Jörg Thalheim <joerg@thalheim.io>
2024-03-02 21:01:38 +0100

    alpabetically sort envlist

--- default.nix
+++ default.nix
@@ -7,12 +7,12 @@
   infinitime = import ./envs/infinitime/shell.nix { pkgs = pkgsUnfree; };
   github-pages = import ./envs/github-pages/shell.nix { inherit pkgs; };
   home-assistant = import ./envs/home-assistant/shell.nix { inherit pkgs; };
-  nannou = import ./envs/nannou/shell.nix { inherit pkgs; };
   jruby = import ./envs/jruby/shell.nix { inherit pkgs; };
+  nannou = import ./envs/nannou/shell.nix { inherit pkgs; };
   openwrt = (import ./envs/openwrt/shell.nix { inherit pkgs; }).env;
   openwrt-ci = import ./envs/openwrt/shell.nix { inherit pkgs; };
   phoronix-test-suite = import ./envs/phoronix-test-suite/shell.nix { inherit pkgs; };
   spec-benchmark = import ./envs/spec-benchmark/shell.nix { inherit pkgs; };
-  yocto = import ./envs/yocto/shell.nix { inherit pkgs; };
   xilinx-vitis = import ./envs/xilinx-vitis/shell.nix { inherit pkgs; };
+  yocto = import ./envs/yocto/shell.nix { inherit pkgs; };
 }
