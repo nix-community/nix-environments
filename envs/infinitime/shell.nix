05742f77c939fe259241212c4a738395456fb7f2
Jörg Thalheim <joerg@thalheim.io>
2024-11-15 09:18:25 +0100

    inifitime: move python.imgtool to mcuboot-imgtool

--- envs/infinitime/shell.nix
+++ envs/infinitime/shell.nix
@@ -10,12 +10,12 @@ pkgs.mkShell {
     cmake
     gcc10
     adafruit-nrfutil
+    mcuboot-imgtool
     (python3.withPackages(python: [
       python.cbor
       python.intelhex
       python.click
       python.cryptography
-      python.imgtool
       python.pillow
     ]))
     nodePackages.lv_font_conv
