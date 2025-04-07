817c3fe21ac952a99548c8b99473faa22865bce0
Jess <jess@jessie.cafe>
2025-04-08 09:01:05 +1200

    ladybird: init

---
+++ envs/ladybird/shell.nix
@@ -0,0 +1,27 @@
+{
+  pkgs,
+  extraPkgs ? [],
+  ...
+}:
+pkgs.mkShell {
+  name = "ladybird-env";
+
+  inputsFrom = [
+    (pkgs.ladybird.override (prev: {
+      skia = prev.skia.overrideAttrs (prev: {
+        gnFlags = prev.gnFlags ++ [
+          "extra_cflags_cc=[\"-frtti\"]"
+        ];
+      });
+    }))
+  ];
+
+  packages = with pkgs; [
+    ccache
+    clang-tools
+    nodePackages.prettier
+  ] ++ extraPkgs;
+
+  # https://github.com/NixOS/nixpkgs/blob/79a8a723b9/pkgs/by-name/la/ladybird/package.nix#L144-L147
+  NIX_LDFLAGS = "-lGL";
+}
